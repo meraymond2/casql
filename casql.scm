cc8581ecca76f50571f6ad59785638ce7ea86943
Michael Raymond <raymond.michael@gmail.com>
2019-09-22 14:18:26 +0100

    Working output json

---
+++ casql.scm
@@ -0,0 +1,54 @@
+(import (chicken process-context))
+(import medea)
+(import postgresql)
+(import sql-null)
+(import srfi-1) ;; lists
+(import srfi-18)
+(import srfi-69) ;; hashtables
+
+;; Postgres ;;
+(define hard-coded-conn-str "postgres://root@localhost/api-db?sslmode=disable")
+
+(define (connect-to-db conn-str)
+  (connect conn-str))
+
+(define (sql-null-unparser v)
+  (write-json 'null))
+
+(json-unparsers (cons (cons sql-null? sql-null-unparser) (json-unparsers)))
+
+(define (sql-list-unparser v)
+  (write-json ))
+;; // Postgres
+
+;; Debug Helpers ;;
+(define (pprint x)
+  (cond
+   ((list? x)
+    (for-each (lambda (i)
+                (print i))
+              x))
+   ((hash-table? x)
+    (hash-table-walk x
+                     (lambda (k v)
+                       (print k " : " v))))
+   ('else
+    (print x))))
+;; // Debug Helpers
+
+;; Core CLI ;;
+(define [main args]
+  (let* [(db-conn (connect-to-db hard-coded-conn-str))
+         (res     (query db-conn "SELECT * FROM cats LIMIT 10"))
+         (rows    (fold (lambda (idx acc)
+                          (vector-set! acc idx (row-alist res idx))
+                          acc)
+                        (make-vector (row-count res))
+                        (iota (row-count res) 0 1)))]
+
+    (write-json rows)
+    (print "\n")
+    ))
+
+;; Run Program ;;
+(main (command-line-arguments))
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 # Requirements
 install libpq. on arch `pacman -S postgresql-libs`
-install the postgres egg `chicken-install postgresql`
+install the postgres egg `chicken-install postgresql` // todo add other egs
+
+# To compile
+`chicken-csc casql.scm -o casql`
