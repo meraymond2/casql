51f8af36170d885aed766ee42ecac48c05a9dd80
Michael Raymond <raymond.michael@gmail.com>
2019-09-25 22:52:38 +0100

    Use args to read options

--- casql.scm
+++ casql.scm
@@ -4,12 +4,9 @@
 (import sql-null)
 (import srfi-1) ;; lists
 (import srfi-18)
-(import srfi-69) ;; hashtables
 (import args)
 
 ;; Postgres ;;
-(define hard-coded-conn-str "postgres://root@localhost/api-db?sslmode=disable")
-
 (define (ts-parser val)
   "For now, just pass through date-string.
    Todo: figure out how to determine if the column
@@ -25,6 +22,44 @@
                                 (default-type-parsers)))
     connection))
 
+(define default-params
+  (list
+   (cons 'host     "localhost")
+   (cons 'port     5432)
+   (cons 'dbname   "")
+   (cons 'user     "postgres")
+   (cons 'password "")
+   (cons 'sslmode  "prefer")
+   ))
+
+(define (alist-merge alist-1 alist-2)
+  "Keys that exist in both alists will be taken from the first,
+   like hash-table-merge."
+  (fold (lambda (pair acc)
+          (if (and (cdr pair) (not (null? (cdr pair))))
+              (alist-update (car pair) (cdr pair) acc)
+              acc))
+        alist-2 alist-1))
+
+(define (run-query q options)
+  (let* ((conn-params (alist-merge (args->conn-params options)
+                                   (if (alist-ref '--read-connection options)
+                                       (load-connection (alist-ref '--read-connection options))
+                                       default-params)))
+         (db-connection (connect-to-db conn-params))
+         (query-result (query db-connection q))
+         (rows (fold (lambda (idx acc)
+                       (vector-set! acc idx (row-alist query-result idx))
+                       acc)
+                     (make-vector (row-count query-result))
+                     (iota (row-count query-result) 0 1)))
+         )
+    (write-json rows)
+    (print " ")
+    (disconnect db-connection)
+    )
+  )
+
 ;; Medea ;;
 (define (sql-null-unparser v)
   (write-json 'null))
@@ -34,97 +69,78 @@
   (json-unparsers (cons (cons sql-null? sql-null-unparser)
                         (json-unparsers))))
 
-(define (sql-list-unparser v)
-  (write-json 'null))
 ;; // Postgres
 
 ;; Saved Connections ;;
 (define (list-connections)
   (print "connections, yada"))
 
-(define (save-connection)
-  (print "Saved connection, etc."))
+(define (save-connection name args)
+  (print "Saved connection, etc. " name " | " args))
 
-(define (delete-connections conns)
-  (for-each (lambda (conn) (print "Deleting " conn)) conns))
+(define (delete-connection name)
+  (print "Deleting connection: " name))
+
+(define (load-connection name)
+  (list))
 
 ;; // Saved Connections ;;
 
-;; Debug Helpers ;;
-(define (pprint x)
-  (cond
-   ((list? x)
-    (for-each (lambda (i) (print i)) x))
-
-   ((hash-table? x)
-    (hash-table-walk x
-                     (lambda (k v)
-                       (print k " : " v))))
-   ('else
-    (print x))))
-;; // Debug Helpers
-
 ;; Arg Parsing ;;
-(define default-args
-  (let [[ht (make-hash-table)]]
-    (hash-table-set! ht "query" #f)
-    (hash-table-set! ht "save" #f)
-    (hash-table-set! ht "--use" #f)
-    (hash-table-set! ht "--host" #f)
-    (hash-table-set! ht "--port" 5432)
-    (hash-table-set! ht "--database" #f)
-    (hash-table-set! ht "--user" #f)
-    (hash-table-set! ht "--password" "")
-    (hash-table-set! ht "--sslmode" "prefer")
-    ht))
+(define list-conn-cmd "list-connections")
+(define save-conn-cmd "save-connection")
+(define del-conn-cmd  "delete-connection")
 
-(define usage
-  "Usage, fill this in later...")
-
-(define list->hash-table
-  (case-lambda
-    ((list) (list->hash-table list (make-hash-table)))
-    ((list initial)
-     (define (loop acc remaining)
-       (if (>= (length remaining) 2)
-           (let* [[k (car remaining)]
-                  [v (cadr remaining)]
-                  [tail (cddr remaining)]]
-             (hash-table-set! acc k v)
-             (loop acc tail))
-           acc))
-     (loop initial list))))
-
-(define (args->conn-params arg-hash)
-  `((host     . ,(hash-table-ref arg-hash "--host"))
-    (port     . ,(string->number (hash-table-ref arg-hash "--port")))
-    (dbname   . ,(hash-table-ref arg-hash "--database"))
-    (user     . ,(hash-table-ref arg-hash "--user"))
-    (password . ,(hash-table-ref arg-hash "--password"))
-    (sslmode  . ,(hash-table-ref arg-hash "--sslmode"))
-    ))
+(define opts
+  (list (args:make-option (q query) #:required "query to execute")
+        (args:make-option (s save) #:required "conn name")
+        (args:make-option (h host) #:required "host name")
+        (args:make-option (p port) #:required "port #"
+                          (set! arg (string->number (or arg "5432"))))
+        (args:make-option (d database) #:required "database name")
+        (args:make-option (u user) #:required "user name")
+        (args:make-option (p password) #:required "password")
+        (args:make-option (sslmode) #:required "ssl mode")
+        ))
 
 
+(define (print-usage)
+  (print (args:usage opts)))
 
+(define (args->conn-params options)
+  (list
+   (cons 'host     (alist-ref 'host options))
+   (cons 'port     (alist-ref 'port options))
+   (cons 'dbname   (alist-ref 'database options))
+   (cons 'user     (alist-ref 'user options))
+   (cons 'password (alist-ref 'password options))
+   (cons 'sslmode  (alist-ref 'sslmode options))
+   ))
 ;; // Arg parsing
-
 
 ;; Core CLI ;;
 (define [main args]
   (init-medea)
-                                        ;(let* [[arg-hash (list->hash-table args)]
-                                        ;       [params   (args->conn-params arg-hash)]
-                                        ;       [db-conn  (connect-to-db params)]
-                                        ;       (res      (query db-conn (hash-table-ref arg-hash "query")))
-                                        ;       (rows     (fold (lambda (idx acc)
-                                        ;                         (vector-set! acc idx (row-alist res idx))
-                                        ;                         acc)
-                                        ;                       (make-vector (row-count res))
-                                        ;                       (iota (row-count res) 0 1)))]
-                                        ;  (write-json rows)
-                                        ;  (newline)
-  (parse-args args)
-                                        ;  )
+  (when (null? args) (print-usage) (exit))
+
+  (receive (options commands) (args:parse (command-line-arguments) opts)
+    (cond
+     ((equal? (car commands) list-conn-cmd)
+      (list-connections))
+
+     ((and (equal? (car commands) save-conn-cmd)
+           (not (null? (cdr commands))))
+      (save-connection (cadr commands) options))
+
+     ((and (equal? (car commands) del-conn-cmd)
+           (not (null? (cdr commands))))
+      (delete-connection (cadr commands)))
+
+     ((not (null? (car commands)))
+      (run-query (car commands) options))
+
+     ('else (print-usage))
+     ))
   )
 
 ;; Run Program ;;
