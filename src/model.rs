9d20a43d1bd5c886b2bf6738a00a7d294dec0a3d
Michael Raymond <raymond.michael@gmail.com>
2019-12-07 09:37:25 +0000

    Rough pass over saving to config file

--- src/model.rs
+++ src/model.rs
@@ -1,11 +1,11 @@
-use serde_derive::Serialize;
+use serde_derive::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct ConnOpts {
-    pub host: String,
-    pub password: Option<String>,
-    pub database: String,
-    pub port: u16,
-    pub sql_impl: String,
-    pub user: String,
+  pub host: String,
+  pub password: Option<String>,
+  pub database: String,
+  pub port: u16,
+  pub sql_impl: String,
+  pub user: String,
 }
