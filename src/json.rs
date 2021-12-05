0b21ecfc725782f882da30fa78eda850a553a6ae
Michael Raymond <michael_raymond@fastmail.com>
2021-12-05 22:37:36 +0000

    Go back to writing json directly

--- src/json.rs
+++ src/json.rs
@@ -1,19 +1,19 @@
-use crate::cas_err::CasErr;
-use crate::postgres::row_iter::RowIter;
-use serde::ser::{SerializeSeq, Serializer};
-use std::io::{BufWriter, Write};
-
-pub fn write_json(rows: RowIter) -> Result<(), CasErr> {
-    let stdout = std::io::stdout();
-    let handle = stdout.lock();
-    let mut buf_writer = BufWriter::new(handle);
-    let mut ser = serde_json::Serializer::new(&mut buf_writer);
-    let mut seq = ser.serialize_seq(None)?;
-    for row in rows {
-        seq.serialize_element(&row)?;
-    }
-    seq.end()?;
-    buf_writer.write("\n".as_bytes())?;
-    buf_writer.flush()?;
-    Ok(())
-}
+// use crate::cas_err::CasErr;
+// use crate::postgres::row_iter::RowIter;
+// use serde::ser::{SerializeSeq, Serializer};
+// use std::io::{BufWriter, Write};
+//
+// pub fn write_json(rows: RowIter) -> Result<(), CasErr> {
+//     let stdout = std::io::stdout();
+//     let handle = stdout.lock();
+//     let mut buf_writer = BufWriter::new(handle);
+//     let mut ser = serde_json::Serializer::new(&mut buf_writer);
+//     let mut seq = ser.serialize_seq(None)?;
+//     for row in rows {
+//         seq.serialize_element(&row)?;
+//     }
+//     seq.end()?;
+//     buf_writer.write("\n".as_bytes())?;
+//     buf_writer.flush()?;
+//     Ok(())
+// }
--- src/postgres/row_iter.rs
+++ src/postgres/row_iter.rs
@@ -1,12 +1,7 @@
 use crate::cas_err::CasErr;
-use crate::cas_val::CasVal;
 use crate::postgres::backend_msgs;
-use crate::postgres::backend_msgs::BackendMsg;
+use crate::postgres::backend_msgs::{BackendMsg, Field};
 use crate::postgres::msg_iter::MsgIter;
-use crate::postgres::types::{parser_generator, ParseClosure};
-use serde::Serialize;
-use serde_with::serde_as;
-use std::collections::HashMap;
 
 // The messages arrive from Postgres in the following order:
 // ParseComplete
@@ -18,18 +13,11 @@ use std::collections::HashMap;
 // ReadyForQuery
 pub struct RowIter<'msgs> {
     msgs: &'msgs mut MsgIter<'msgs>,
-    parse: ParseClosure,
+    pub fields: Vec<Field>,
 }
 
-#[serde_as]
-#[derive(Debug, Serialize)]
-pub struct RowVals(#[serde_as(as = "HashMap<_, _>")] pub Vec<(String, CasVal)>);
-
 impl<'msgs> RowIter<'msgs> {
-    pub fn from(
-        msgs: &'msgs mut MsgIter<'msgs>,
-        dynamic_types: HashMap<i32, String>,
-    ) -> Result<Self, CasErr> {
+    pub fn from(msgs: &'msgs mut MsgIter<'msgs>) -> Result<Self, CasErr> {
         let mut fields = Vec::new();
         while let Some(msg) = msgs.next() {
             match backend_msgs::type_of(&msg) {
@@ -51,21 +39,18 @@ impl<'msgs> RowIter<'msgs> {
                 }
             }
         }
-        Ok(RowIter {
-            msgs,
-            parse: parser_generator(fields, dynamic_types),
-        })
+        Ok(RowIter { msgs, fields })
     }
 }
 
 impl<'msgs> Iterator for RowIter<'msgs> {
-    type Item = RowVals;
+    type Item = Vec<u8>;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.msgs
             .next()
             .and_then(|msg| match backend_msgs::type_of(&msg) {
-                BackendMsg::DataRow => Some(backend_msgs::parse_data_row(&msg, &mut self.parse)),
+                BackendMsg::DataRow => Some(msg),
                 BackendMsg::Close => self.next(),
                 BackendMsg::ReadyForQuery => None, // finished
                 _ => {
